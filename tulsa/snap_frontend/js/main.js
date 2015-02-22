c627089b98af4560f992a70e03f92ae91456beba
Jerry Mizer <jmizer@tulsaconnect.com>
2015-02-21 22:12:15 -0600

    Add some logic to make the Yes/No questions work
    Add logic to main.js to support Yes/No question to be evaluated according to the
    desired answer
    
    Add logic to main.js to support template selection and configuration based
    upon the questions.json file. The idea here is that the configuring user
    may use an existing template or may add an additional template to support
    their question/answer scenarios.
    
    Add a section called non_question_responses to questions.json to provide
    a lookup for unqualified_person template name. IMO this section may be
    leveraged further to provide custom responses in scenarios where that is
    required.
    
    Change questions.json to reflect the problem space a bit better. (IMO)
    
    Add a template for unqualified_person that shows a message that says user
    may not be qualified and provides a link to the qualifications for
    Oklahoma.

--- tulsa/snap_frontend/js/main.js
+++ tulsa/snap_frontend/js/main.js
@@ -5,11 +5,13 @@
     }
 
     var templates = {
-        YesNo: _.template($('#yes_no_input').html()),
+        YesNoButton: _.template($('#yes_no_input').html()),
         input: _.template($('#default_input').html()),
+        unqualified_person: _.template($('#unqualified_person').html())
+
     };
 
-    function QuestionController(questions, options) {
+    function QuestionController(questions, non_question_responses, options) {
         if (typeof options === null) {
             options = {};
         }
@@ -18,14 +20,30 @@
             back_button: "#back_btn"
         });
         this.questions = questions;
+        this.non_question_responses = non_question_responses;
         this.answers = {};
         this.index = 0;
         this.history = [];
+        this.failed = false;
     }
+
+    QuestionController.prototype.is_survey_failed = function(){
+        return this.failed;
+    };
 
     QuestionController.prototype.get_question = function (name) {
         return _.findWhere(this.questions, {name: name});
     };
+
+    QuestionController.prototype.navigate = function (current_question) {
+        // get either the next question when succesfully validated
+        // or the actual next step
+       if(!this.is_survey_failed()) {
+           next_view_name = current_question.next_pass;
+           this.index = _.indexOf(this.questions, this.get_question(next_view_name));
+       }
+    };
+
 
     QuestionController.prototype.next_question = function (current_question) {
         // get either the next question when succesfully validated
@@ -39,6 +57,10 @@
     };
 
     QuestionController.prototype.get_current = function () {
+        /*This section could grow to include other responses or could go away.*/
+        if(this.failed)
+            return _.findWhere(this.non_question_responses, {name: 'unqualified_person'});
+
         return this.questions[this.index];
     };
 
@@ -55,6 +77,55 @@
         }
     };
 
+    QuestionController.prototype.process_template = function(current_question, store_value)
+    {
+        this.store_value(current_question.name, store_value);
+        this.navigate(current_question);
+        this.render();
+    };
+
+    QuestionController.prototype.create_template = function(template_type, context_object){
+        var template = templates[template_type];
+        var template_with_context = $(template(context_object));
+        return template_with_context;
+    };
+
+    QuestionController.prototype.configure_template = function(controller, current_component, target_template)
+    {
+        /*The Idea here is to be able to configure each template based upon the question's configuration and not just the template.
+        * So if we have a YesNoButton and the expected answer is yes, then the class of the clicked button must be yes-btn
+        * This same scenario will work for other groups of buttons (Yes, No, Maybe) as long as the classes on the buttons in the template
+        * stay in sync.
+        *
+        * In my opinon the current questions.json file is more appropriate for a dynamic survey, but that is just my opinion*/
+
+        switch (current_component.template_type)
+        {
+            case 'YesNoButton':
+                target_template.on('click', '.action', function(e){
+                    e.preventDefault();
+                    var correct_answer_button_type = current_component.expected_answer + '-btn';
+                    controller.failed = !$(e.currentTarget).hasClass(correct_answer_button_type);
+                    controller.process_template(current_component, !controller.failed);
+                    return false;
+                });
+                break;
+            case 'input':
+                target_template.on('click', '.btn-next', function(e){
+                    e.preventDefault();
+                    var val = tmpl.find('.input_value');
+                    val_el = val[0];
+                    if (val_el.checkValidity && !val_el.checkValidity()) {
+                        // html5 validate, if a special type is specified
+                        return false;
+                    }
+                    self.process_template(current_component, val.val());
+                    return false;
+                });
+                break;
+        }
+    }
+
     QuestionController.prototype.render_current = function () {
         var self, current, current_value;
         self = this;
@@ -65,41 +136,15 @@
             value: current_value
         }, current);
 
-        template = templates[current.question_type];
-        tmpl = $(template(context));
+       var template = this.create_template(current.template_type, context);
         // terrible but wat
         // handle yes/no actions
-        if (current.question_type == 'YesNo') {
-            tmpl.on('click', '.action', function(e){
-                e.preventDefault();
-                if ($(e.currentTarget).hasClass('yes-btn')) {
-                    self.store_value(current.name, true);
-                    self.next_question(current);
-                    self.render();
-                }else{
-                    self.store_value(current.name, false);
-                    self.fail();
-                }
-                return false;
-            });
-        }else if(current.question_type == 'input'){
-            tmpl.on('click', '.btn-next', function(e){
-                e.preventDefault();
-                var val = tmpl.find('.input_value');
-                val_el = val[0];
-                if (val_el.checkValidity && !val_el.checkValidity()) {
-                    // html5 validate, if a special type is specified
-                    return false;
-                }
-                self.store_value(current.name, val.val());
-                self.next_question(current);
-                self.render();
-                return false;
-            });
-        }
-        return tmpl;
-    };
+        this.configure_template(self, current, template);
 
+
+        return template;
+
+    };
     QuestionController.prototype.render = function () {
         $(this.options.render_div).html(this.render_current());
     };
@@ -108,8 +153,8 @@
 
     $(function() {
         // Start this up
-        $.getJSON('../Simple/questions.json').done(function(resp) {
-            w.questions = new QuestionController(resp.questions, {});
+        $.getJSON('../configuration/questions.json').done(function(resp) {
+            w.questions = new QuestionController(resp.questions, resp.non_question_responses, {});
             w.questions.render();
             region = resp.region;
         }).fail(function(resp, textStatus, error) {
